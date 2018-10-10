cfb0badc3f58d3b85f53bd670d84f050fe9d659c
Oliver Wen <wb493797144@gmail.com>
2018-10-10 16:01:11 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181010055543) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
     t.string "course_description"
