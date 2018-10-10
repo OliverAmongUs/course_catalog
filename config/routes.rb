122fecc2dedc789f782dc605e6f1326f3e357873
Oliver Wen <wb493797144@gmail.com>
2018-10-10 16:31:04 -0400

    build home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/welcome'
+
   resources :instructors
   resources :subjects
   resources :courses
-  root 'courses#index'
+  root 'home#welcome'
   # root 'subjects#index'
   # root 'instructors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
